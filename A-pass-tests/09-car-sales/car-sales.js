e38f8a7a3d58767de21dde602208e3b2a2ddc84a
Kawa MANMI <kawamam2007@gmail.com>
2022-11-09 18:41:36 +0000

    Car-Sale has  been done

--- A-pass-tests/09-car-sales/car-sales.js
+++ A-pass-tests/09-car-sales/car-sales.js
@@ -1,3 +1,10 @@
-function sales(carsSold) {}
+function sales(carsSold) {
+  totalSoldCars = {};
+  carsSold.map((arrayElm) => {
+    totalSoldCars[arrayElm.make] =
+      (totalSoldCars[arrayElm.make] || 0) + arrayElm.price;
+  });
+  return totalSoldCars;
+}
 
 module.exports = sales;
