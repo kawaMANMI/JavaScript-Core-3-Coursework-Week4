a825e461a5bcfdd4f5c8e817bbace76b86632366
Kawa MANMI <kawamam2007@gmail.com>
2022-11-09 19:16:45 +0000

    Remove vowels  has  been done

--- B-write-tests/03-remove-vowels-from-array/remove-vowels-in-array.test.js
+++ B-write-tests/03-remove-vowels-from-array/remove-vowels-in-array.test.js
@@ -2,8 +2,13 @@ let removeVowelsFromWords = require("./remove-vowels-in-array");
 
 test("remove vowels from all words in array", function () {
   // Arrange
+  let words = ["Irina", "Etza", "Daniel"];
+  let output = removeVowelsFromWords(words);
   // Act
+  let expectedResult = ["rn", "tz", "Dnl"];
   // Assert
+
+  expect(output).toEqual(expectedResult);
 });
 
 // example
