65bc7093597b5b306e845ffafd198338574bdd4f
Kawa MANMI <kawamam2007@gmail.com>
2022-11-11 01:43:52 +0000

    step3 in add function not completed as I could completely understand Tothrow in Jest

--- C-TDD/03-calculator/add.js
+++ C-TDD/03-calculator/add.js
@@ -1,14 +1,20 @@
 function add(numbers) {
   let sum = 0;
   if (numbers != "") {
-    const arrayNumbers = numbers.split(",");
+    const arrayNumbers = numbers.split(",").map((num) => parseInt(num));
+    const negativeNumbers = arrayNumbers.filter((num) => num < 0);
+    if (negativeNumbers.length > 0) {
+      return `negatives not allowed: ${negativeNumbers} `;
+    }
     for (let i = 0; i < arrayNumbers.length; i++) {
-      sum += parseInt(arrayNumbers[i]);
+      if (arrayNumbers[i] <= 1000) {
+        sum += arrayNumbers[i];
+      }
     }
   }
   return sum;
 }
 
-module.exports = add;
+// module.exports = add;
 
-// console.log(add("4,5"));
+console.log(add("3, 5, -2"));
--- C-TDD/03-calculator/add.test.js
+++ C-TDD/03-calculator/add.test.js
@@ -4,7 +4,15 @@ test("Sum of the numbers in a string", function () {
   // Arrange
   const numbers1 = "";
   output1 = add(numbers1);
-  expect(output1).toEqual(1);
+  expect(output1).toEqual(0);
+
+  const numbers2 = "3, 5, 9";
+  output2 = add(numbers2);
+  expect(output2).toEqual(17);
+
+  const numbers3 = "3, 5, 9, 1003";
+  output3 = add(numbers3);
+  expect(output3).toEqual(17);
 
   // Act
   // Assert
